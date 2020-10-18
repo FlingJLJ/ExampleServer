3ca9c7b38edba9c7e3e70b05d1827e896f54fb01
FlingJLJ <60308125+FlingJLJ@users.noreply.github.com>
2020-10-17 22:09:41 -0500

    more

--- server.js
+++ server.js
@@ -7,17 +7,15 @@ console.log("Visit \"localhost:80\" in your browser to connect to this server.")
 
 http.createServer((request, response) =>
 {
-    console.log("Good job, you visited the page being hosted on this server!")
     try
     {
         response.writeHead(200)
         response.write(fs.readFileSync("index.html"))
-        response.end()
     }
     catch(error)
     {
         response.write("<h1>Oops! There was an error processing your request.</h1><hr><p>More error info:</p><br>" + error)
-        response.end()
         console.log(error)
     }
+    response.end()
 }).listen(port)
